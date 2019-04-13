66a15ef73a88ddec0eee8b0c21d99879990289b4
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-04-13 11:18:50 -0700

    new changes to see a second algorithm

--- src/main/java/com/lambda/spring/basics/springin5steps/BinarySearchimp.java
+++ src/main/java/com/lambda/spring/basics/springin5steps/BinarySearchimp.java
@@ -2,6 +2,10 @@ package com.lambda.spring.basics.springin5steps;
 
 public class BinarySearchimp {
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
+		
+		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+		
+		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
 		//Implementing Sorting Logic
 		//Bubble Sort Algorithm
 		//Search the array
